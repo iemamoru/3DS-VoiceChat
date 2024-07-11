80cb32bafebd1b3d1d1c0c9a91bf9505589e1c08
iemamoru <josemendoxe@gmail.com>
2024-07-12 03:00:52 +0900

    create socket system

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -2,13 +2,14 @@
 #include "csvc.h"
 #include <CTRPluginFramework.hpp>
 #include "buffers.h"
-
+#include "patches.h"
+#include "debug.h"
 #include <vector>
 
 namespace CTRPluginFramework
 {
     // This patch the NFC disabling the touchscreen when scanning an amiibo, which prevents ctrpf to be used
-    static void    ToggleTouchscreenForceOn(void) {
+    static void ToggleTouchscreenForceOn(void) {
         static u32 original = 0;
         static u32 *patchAddress = nullptr;
 
@@ -53,9 +54,42 @@ exit:
         svcCloseHandle(processHandle);
     }
 
-    // This function is called before main and before the game starts
-    // Useful to do code edits safely
-    void    PatchProcess(FwkSettings &settings) {
+    void ProccessEventCallback(Process::Event event) {
+        
+        if (event == Process::Event::EXIT) {
+            socExit();
+
+            u32 temp;
+            svcControlMemoryUnsafe((u32 *)&temp, SHAREDMEM_ADDR, SERVICE_SHAREDMEM_SIZE * SERVICE_COUNT, (MemOp)MEMOP_FREE, (MemPerm)0);
+        }
+    }
+    
+    void PatchProcess(FwkSettings &settings) {
+        Process::SetProcessEventCallback(ProccessEventCallback);
+
+        if (System::IsCitra()) {
+            Result res;
+
+            res = socInit((u32 *)0, SERVICE_SHAREDMEM_SIZE);
+            DEBUG_NOTIFY(Utils::Format("Soc Init: %08lX", res));
+        } else {
+            u32 temp;
+            Result res;
+
+            if(R_FAILED(res = svcControlMemoryUnsafe((u32 *)&temp, SHAREDMEM_ADDR, SERVICE_SHAREDMEM_SIZE * SERVICE_COUNT, MemOp(MEMOP_REGION_SYSTEM | MEMOP_ALLOC), MemPerm(MEMPERM_READ | MEMPERM_WRITE))))
+            {
+                MessageBox(Utils::Format("Failed to allocate memory. (Error code: %08lX)\nWe are going to abort.", res));
+                abort();
+            }
+
+            res = socInit((u32 *)SHAREDMEM_ADDR, SERVICE_SHAREDMEM_SIZE);
+            DEBUG_NOTIFY(Utils::Format("socInit: %08lX", res));
+        }
+
+        if(!InstallSocketHooks())
+        {
+            OSD::Notify("Failed to install socket hooks", Color::Red);
+        }
         ToggleTouchscreenForceOn();
     }
 
